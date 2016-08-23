a7b94cd6c0dae945af5d99f92fcbc47ddc7de5db
Michael Truog <mjtruog@gmail.com>
2016-08-23 12:27:08 -0700

    Remove old version files.

--- old/java/BookUtilities/deploy.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-curl -X POST -d @BookUtilities.conf http://localhost:6464/cloudi/api/rpc/services_add.erl
-
--- old/java/BookUtilities/test.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-# Run the different utility services
-#curl http://localhost:6464/book/utility/load_catalog
-curl http://localhost:6464/book/utility/generate_ratings
-#curl http://localhost:6464/book/utility/generate_item_attributes
-#curl http://localhost:6464/book/utility/load_predictions
-
